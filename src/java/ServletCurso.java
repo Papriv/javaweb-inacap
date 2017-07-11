5644bb2ccfcb0a0a1126bc653f947c6db4bb1d1a
yodebian <yodebian@RICARDOTOLEA087>
2017-07-10 20:25:12 -0400

    Esto es una prueba para git

--- src/java/ServletCurso.java
+++ src/java/ServletCurso.java
@@ -1,3 +1,4 @@
+
 import cl.negocio.Course;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -6,47 +7,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class ServletCurso extends HttpServlet {
-
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            Course course=new Course();
-            if(request.getParameter("delete")==null ){
-                out.println("DELETE NULO");
-            }else{
-                String id=request.getParameter("delete");
+            Course course = new Course();
+            if (request.getParameter("delete") == null) {
+
+                if (request.getParameter("accion").equals("registrar")) {
+                    String name = request.getParameter("name");
+                    String description = request.getParameter("description");
+                    course.setName(name);
+                    course.setDescription(description);
+                    course.setState("activo");
+                    course.save();
+                    response.sendRedirect("cursos/index.jsp");
+
+                } else if (request.getParameter("accion").equals("actualizar")) {
+                    String id = request.getParameter("id");
+                    String name = request.getParameter("name");
+                    String description = request.getParameter("description");
+                    course.setId(id);
+                    course.setName(name);
+                    course.setDescription(description);
+                    course.setState("activo");
+                    course.update();
+                    response.sendRedirect("cursos/index.jsp");
+                }
+
+            } else {
+                String id = request.getParameter("delete");
                 course.setId(id);
                 course.delete();
                 out.println(" Su registro ha sido eliminado !!! <a href=cursos/index.jsp>Volver </a>");
             }
-                    
-            if(request.getParameter("accion").equals("registrar")){
-                String name=request.getParameter("name");
-                String description=request.getParameter("description");
-                course.setName(name);
-                course.setDescription(description);
-                course.setState("creado");
-                course.save();
-              
-            }
-            else if(request.getParameter("accion").equals("actualizar")){
-                String id=request.getParameter("id");
-                String name=request.getParameter("name");
-                String description=request.getParameter("description");
-                course.setId(id);
-                course.setName(name);
-                course.setDescription(description);
-                course.setState("activo");
-                course.update();
-                response.sendRedirect("cursos/index.jsp");
-            }
-            
-            
-            
+
             /*else{
                 String id=request.getParameter("del");
                 course.setId(id);
